35cbd65fdad23ae9e4c9cb3d2818ee85d94d4a26
karthikk-com <karthik@karthikk.com>
2023-12-11 16:12:56 +0530

    Patterns 6

---
+++ Patterns/pattern6.js
@@ -0,0 +1,21 @@
+/**
+ * Pattern 6
+ * 
+ * 12345
+ * 1234
+ * 123
+ * 12
+ * 1
+ */
+
+function printPattern(n){
+    for(let i=1; i<=n; i++){
+        for(let j=1; j<=n-i+1; j++){
+            process.stdout.write(j+'');
+        }
+
+        process.stdout.write('\n');
+    }
+}
+
+printPattern(5);
