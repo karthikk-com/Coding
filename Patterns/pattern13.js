5f045fb3b5862b5a7d88a40eabc033d0d54acd67
karthikk-com <karthik@karthikk.com>
2023-12-14 11:50:12 +0530

    pattern13

---
+++ Patterns/pattern13.js
@@ -0,0 +1,21 @@
+/* Pattern 13
+N=5
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
+
+function printPattern(n){
+    let number=1
+    for(let i=1; i<=n; i++){
+        for(let j=1; j<=i; j++){
+            process.stdout.write(number+' ')
+            number++;
+        }
+        process.stdout.write('\n');
+    }
+}
+
+printPattern(3);
