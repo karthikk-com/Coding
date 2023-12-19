3f2616bf3bf86b558095c18a1c7033c06412eb40
karthikk-com <karthik@karthikk.com>
2023-12-19 12:07:36 +0530

    Updating inference

--- MathProbs/CountDigits.js
+++ MathProbs/CountDigits.js
@@ -5,18 +5,31 @@ Output: 5
 Explanation: N has 5 digits
 */
 
-/* Inference :
-Solve by using extraction of digits using division and looping the floor value */
+/*
+Edge case : Octa Literals does not support i.e passing 012322 will not return the expected output.
+Edge Case : CHeck if the passed input is a valid number.
+use typeof(inputValue) -> to check if the input is a number.
+use Number.isInteger()  -> ES6 Built in to check if the input is a integer and not contain any decimal value.
+
+Inference :
+Solve by using extraction of digits using division and looping the floor value 
+Using extraction of digit method, we remove the last digit by dividing the input number by 10 and remove the decimal value until the input reaches 0
+Also, make use of a counter at each iteration to know the number of digits.
+*/
+
 function countTheDigit(n){
-    let counter = 0;
-    let x =n;
-
-    while(x>0){
-        x=Math.floor(x/10);
-        counter++;
+    if(Number.isInteger(num)){
+        let counter = 0;
+        let x =n;
+        
+        while(x>0){
+            x=Math.floor(x/10);
+            counter++;
+        }
+        return counter;
     }
-
-    return counter;
+    return "Not a number"
+          
 }
 
 console.log(countTheDigit(1232));
