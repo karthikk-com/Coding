859a2a9b06bd3b08a1d89619aaba7276f766265c
karthikk-com <karthik@karthikk.com>
2023-12-20 12:10:34 +0530

    Optimzed approach -O(√n)

--- MathProbs/PrimeNumber.js
+++ MathProbs/PrimeNumber.js
@@ -23,3 +23,21 @@ function isPrime(n){
 
 console.log(isPrime(4));
 
+/* We learnt that we can detect all the factors just by squareroot of (N) in print all divisors problem*/
+//O(√n)
+function isPrime2(n){
+    let counter = 0;
+
+    for(let i=1; i<=Math.sqrt(n); i++){
+        if(n%i===0){
+            counter++;
+            if(n/i != i)
+                counter++;
+        }
+    }
+
+    return (counter === 2);
+}
+
+console.log(isPrime2(11));
+console.log(isPrime2(8));
