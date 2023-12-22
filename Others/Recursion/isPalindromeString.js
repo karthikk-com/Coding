cb7751a636d74cfff16cc18d57898479f3316660
karthikk-com <karthik@karthikk.com>
2023-12-22 09:40:12 +0530

    Check if the string is a palindrom or not

---
+++ Others/Recursion/isPalindromeString.js
@@ -0,0 +1,23 @@
+/* 
+Check if the string is a palindrome
+
+Input : MADAM
+Output : true
+
+Input : MADIM
+Output : false
+*/
+
+function isPalindromeString(str, i,len){
+//Base Condition
+if(i>=parseInt(len/2))
+    return true;
+
+//Task
+if(str.charAt(i) !== str.charAt(len-i-1))
+    return false
+
+return isPalindromeString(str,i+1,len);
+}
+
+console.log(isPalindromeString("MADIM",0,5));
