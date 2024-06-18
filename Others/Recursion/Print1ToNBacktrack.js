342b22d4c0ba04fa7c7d052459394757f385db2d
karthikk-com <karthik@karthikk.com>
2024-06-18 11:23:13 +0530

    diff in output when using i-- with that of i-1

--- Others/Recursion/Print1ToNBacktrack.js
+++ Others/Recursion/Print1ToNBacktrack.js
@@ -1,17 +1,26 @@
 /* 
 Print from 1 to N using recursion back tracking
 
-i.e Executing the last function call, and decreamenting to first function call
+i.e Executing the last function call, and decrementing to first function call
 */
 
-function PrintFrom1ToNBacktrack(i,n){
+function PrintFrom1ToNBacktrack(i){
     //base case
     if(i<1)
         return;
 
-    PrintFrom1ToNBacktrack(i-1,n);
+    PrintFrom1ToNBacktrack(i-1);
     process.stdout.write(i +" ");
 }
 
-PrintFrom1ToNBacktrack(3,3);
+PrintFrom1ToNBacktrack(5); // 1 2 3 4 5
 
+
+function printReverse(i){
+    if(i<1)
+        return;
+    printReverse(--i);
+    process.stdout.write(i+" ");
+}
+
+printReverse(5); // 0 1 2 3 4
