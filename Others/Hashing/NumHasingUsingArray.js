0ab8b791ffb9a9aae3209bf773a1456452f6a42e
karthikk-com <karthik@karthikk.com>
2024-06-21 22:35:28 +0530

    Update on max value of an array

---
+++ Others/Hashing/NumHasingUsingArray.js
@@ -0,0 +1,20 @@
+/*  input [1,2,4,2,1,3]
+output :
+1 : 2
+2 : 2
+3 : 1
+4 : 1
+hash map functionality using arrays
+ */
+
+/* Array can hold a size upto 10^6 inside main & 10^7 globally */
+function showFrequency(arr){
+    let newArr=[];
+    for(let ar of arr){
+        newArr[ar]=(newArr[ar]!=undefined) ? ++newArr[ar] : 1;
+    }
+}
+
+showFrequency([1,2,4,2,1,3,0,8]);
+
+
