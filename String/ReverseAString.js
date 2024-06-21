2f77468b7da4501d24366f4f72ed493068b62506
karthikk-com <karthik@karthikk.com>
2024-06-21 22:33:53 +0530

    Using Recursive

--- String/ReverseAString.js
+++ String/ReverseAString.js
@@ -1,3 +1,9 @@
+/* https://leetcode.com/problems/reverse-string/
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory. */
+
 function reverseString(s){
     let mid = Math.floor(s.length/2);
     let n = s.length;
@@ -17,3 +23,16 @@ function reverseString(s){
 
 console.log(reverseString(["h","e","l","l","o"]));
 //console.log(reverseString(["A"," ","m","a","n",","," ","a"," ","p","l","a","n",","," ","a"," ","c","a","n","a","l",":"," ","P","a","n","a","m","a"]))
+
+// Using Recursion
+function recursiveReverseString(i,str){
+    // Base condition
+    let n = str.length;
+    if(i>=Math.floor(n/2))
+        return str;
+
+    [str[i],str[n-i-1]] = [str[n-i-1], str[i]];
+    return recursiveReverseString(i+1,str);
+}
+
+console.log(recursiveReverseString(0,["h","e","l","l"]));
